d43ec0ce79b44641991d2a272e5e58279f240d52
Cazelex <keegan.stroud@outlook.com>
2025-06-22 23:03:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var IsLogedIn = false;
 
+function showToast(message) {
+    const toast = document.getElementById("toast");
+    toast.textContent = message;
+    toast.className = "show";
+    setTimeout(() => {toast.className = toast.className.replace("show", "");}, 3000); // Toast will disappear after 3 seconds
+}   
+
 function downloadFile() {
-    if (IsLogedIn = true) {
+    if (IsLogedIn === true) {
         const link = document.createElement("a");
         link.href = "download.txt";
         link.download = "download.txt";
@@ -9,12 +16,7 @@ function downloadFile() {
         link.click();
         document.body.removeChild(link);
     } else {
-        function showToast(message) {
-            const toast = document.getElementById("toast");
-            toast.textContent = message;
-            toast.className = "show";
-            setTimeout(() => {toast.className = toast.className.replace("show", "");}, 3000); // Toast will disappear after 3 seconds
-        }   
+        showToast("You are noit logged in meaning you are super gay so no download for you!")
     }
 
 }
